20595eede5f1b7becb12fa710a96e256577a28bd
Ajoke32 <ipz211_bmm@student.ztu.edu.ua>
2023-11-11 16:20:44 +0200

    redux setup and data flow migrated to redux

--- src/clientApi/topics/fetchTopics.ts
+++
@@ -1,8 +0,0 @@
-import { TopicType } from '@/types/topicTypes';
-import axios from 'axios';
-
-export const fetchTopics = async (topicType: string) => {
-    const res = await axios.get<TopicType[]>(`/api/topics/${topicType}`);
-
-    return res.data;
-};
